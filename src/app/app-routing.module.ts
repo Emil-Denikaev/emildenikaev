b04b9c510202fc919d19b06d4fdeb123e0ff1142
Denikaev Emil <edenikaev@mail.ru>
2022-10-16 17:17:48 +0400

    Макет сайт

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { MainComponent } from './components/main/main.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'resume', component: ResumeComponent
+  },
+  {
+    path: 'contacts', component: ContactsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HeaderComponent } from './components/header/header.component'; 
+import { MainComponent } from './components/main/main.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AboutComponent,
+    ResumeComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
